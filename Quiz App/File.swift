4155713610f9571d489471bbcb6f965053fc4752
Guan Yi <lee_guan_yi@s2021.ssts.edu.sg>
2022-06-18 10:35:51 +0800

    Initial Commit

---
+++ Quiz App/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Quiz App
+//
+//  Created by Lee Guan Yi on 16/6/22.
+//
+
+import Foundation
+
+struct QuizQuestion{
+    var title: String
+    
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+
+}
---
+++ Quiz App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz App
+//
+//  Created by Lee Guan Yi on 16/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
